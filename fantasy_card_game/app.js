d0a237364b8363b798f31cfd02196c1cfb1917f0
reamono <popo8751jpn@gmail.com>
2025-05-27 13:58:35 +0900

    Update app.js

--- fantasy_card_game/app.js
+++ fantasy_card_game/app.js
@@ -165,7 +165,7 @@ function showDeckChoices() {
     cardElem.className = `card ${rarityClass}`;
     cardElem.innerHTML = `
       <h3>${card.name}</h3>
-      <p>${card.effect}</p>
+      <p>${card.description}</p>
       <p>マナ: ${card.cost}</p>
       <p class="rarity">${card.rarity}</p>
     `;
